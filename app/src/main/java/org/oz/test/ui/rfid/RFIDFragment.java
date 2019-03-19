d14dcd90c97cff512d120d37f05596e95960e463
ozcomcn <857527916@qq.com>
2019-03-20 03:16:09 +0800

    add navigation jetpack

--- app/src/main/java/org/oz/test/ui/rfid/RFIDFragment.java
+++ app/src/main/java/org/oz/test/ui/rfid/RFIDFragment.java
@@ -7,12 +7,18 @@ import android.view.ViewGroup;
 
 import org.oz.test.R;
 import org.oz.test.base.BaseFragment;
+import org.oz.test.databinding.FragmentMainBinding;
+import org.oz.test.databinding.RfidFragmentBinding;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 import androidx.lifecycle.ViewModelProviders;
 
 public class RFIDFragment extends BaseFragment {
+
+    private static int count = 0;
 
     private RfidViewModel mViewModel;
 
@@ -21,15 +27,32 @@ public class RFIDFragment extends BaseFragment {
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mViewModel = ViewModelProviders.of(this).get(RfidViewModel.class);
+
+        mViewModel.count.setValue(count++);
+
+    }
+
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.rfid_fragment, container, false);
+
+        final RfidFragmentBinding binding = DataBindingUtil.inflate(inflater, R.layout.rfid_fragment, container, false);
+
+        binding.setLifecycleOwner(this);
+
+        binding.setVm(mViewModel);
+
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(RfidViewModel.class);
+
         // TODO: Use the ViewModel
     }
 
--- app/src/main/java/org/oz/test/ui/rfid/RfidViewModel.java
+++ app/src/main/java/org/oz/test/ui/rfid/RfidViewModel.java
@@ -4,16 +4,19 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
 
 public class RfidViewModel extends AndroidViewModel {
 
+    public MutableLiveData<Integer> count = new MutableLiveData<>();
+
     public RfidViewModel(@NonNull Application application) {
         super(application);
-
-
     }
 
 
-
-
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+    }
 }
