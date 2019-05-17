57176e4dca45e127928dd9ec5f5e5f0a8a4b057b
coco <857527916@qq.com>
2019-05-17 16:27:09 +0800

    add ripple

--- app/src/main/java/org/oz/test/databinding/adpater/ViewAdpater.java
+++ app/src/main/java/org/oz/test/databinding/adpater/ViewAdpater.java
@@ -1,11 +1,33 @@
 package org.oz.test.databinding.adpater;
 
+import android.graphics.Outline;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+
 import androidx.databinding.BindingAdapter;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.chip.ChipGroup;
 
 public class ViewAdpater {
+
+    @BindingAdapter({"clipToOutline"})
+    public static void clipToOutline(View view, boolean enable) {
+
+        view.setClipToOutline(enable);
+
+        if (enable)
+            view.setOutlineProvider(new ViewOutlineProvider() {
+                @Override
+                public void getOutline(View view, Outline outline) {
+
+                    outline.setOval(new Rect(0, 0, view.getWidth(), view.getHeight()));
+                }
+            });
+
+    }
 
     @BindingAdapter({"onItemSelectedListener"})
     public static void onNavigationItemSelectedListener(BottomNavigationView view, BottomNavigationView.OnNavigationItemSelectedListener listener) {
